f005a8c5a63f719a5318d03a4bc5f23b06cde2fd
Vikiwest <chidiolorunda@gmail.com>
2025-01-13 13:17:48 +0100

    Updated

--- script.js
+++ script.js
@@ -2,45 +2,46 @@
 window.addEventListener("load", function () {
   const loadingScreen = document.getElementById("loading-screen");
   const mainContent = document.getElementById("main-content");
-  
-  setTimeout(() => { 
+
+  setTimeout(() => {
     loadingScreen.style.display = "none";
     mainContent.style.display = "block";
-}, 2000); // Loading screen will stay visible for 2 seconds
+  }, 2000); // Loading screen will stay visible for 2 seconds
 });
 
 // Show loading screen when navigating to another page
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', function(event) {
-      event.preventDefault(); // Prevent the default navigation temporarily
-      document.getElementById("loading-screen").style.display = "flex";
-      setTimeout(() => {
-          window.location.href = this.href; // Redirect after delay
-      }, 1000); // Adjust delay as needed
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default navigation temporarily
+    document.getElementById("loading-screen").style.display = "flex";
+    setTimeout(() => {
+      window.location.href = this.href; // Redirect after delay
+    }, 1000); // Adjust delay as needed
   });
 });
 
-
-
-
-
+//ProductName and Image linked to whatsapp
+function buyNow(productName) {
+  var phoneNumber = "+2348122086954";
+  var message = encodeURIComponent("Hello, I am interested in " + productName);
+  var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + message;
+  window.open(whatsappUrl, "_blank");
+}
 
 //Header
-const header = document.getElementById('mainHeader');
+const header = document.getElementById("mainHeader");
 let lastScrollY = window.scrollY;
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > lastScrollY) {
     // Scrolling down
-    header.classList.add('hidden');
+    header.classList.add("hidden");
   } else {
     // Scrolling up
-    header.classList.remove('hidden');
+    header.classList.remove("hidden");
   }
   lastScrollY = window.scrollY;
 });
-
-
 
 //WELCOME MESSAGE
 function showWelcomeMessage() {
@@ -61,8 +62,6 @@ function showWelcomeMessage() {
 // Call the function on page load
 window.onload = showWelcomeMessage;
 
-
-
 //SIDE BAR
 document.getElementById("sidebarToggle").addEventListener("click", function () {
   document.getElementById("sidebar").style.right = "0"; // Show sidebar
@@ -73,8 +72,6 @@ document.getElementById("closeSidebar").addEventListener("click", function () {
   document.getElementById("sidebar").style.right = "-100%"; // Hide sidebar
   document.body.classList.remove("no-scroll");
 });
-
-
 
 //CURRENT PAGE INDICATOR
 document.addEventListener("DOMContentLoaded", function () {
@@ -92,20 +89,15 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-
 //HIDDEN REVIEWS
 
-document.getElementById('view-more').addEventListener('click', function() {
-  const hiddenReviews = document.querySelector('.hidden-content');
-  if (hiddenReviews.style.display === 'none') {
-    hiddenReviews.style.display = 'block'; 
-    this.textContent = 'View Less '; 
+document.getElementById("view-more").addEventListener("click", function () {
+  const hiddenReviews = document.querySelector(".hidden-content");
+  if (hiddenReviews.style.display === "none") {
+    hiddenReviews.style.display = "block";
+    this.textContent = "View Less ";
   } else {
-    hiddenReviews.style.display = 'none'; 
-    this.textContent = 'View More '; 
+    hiddenReviews.style.display = "none";
+    this.textContent = "View More ";
   }
-  
 });
-
-
-
